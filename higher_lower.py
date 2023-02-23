c38e6a804af4d891bfe867632a4ec352900035ed
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-02-23 11:06:25 +0100

    test

---
+++ higher_lower.py
@@ -0,0 +1,4 @@
+# TODO: Generate a random number of followers for each element in the list
+# TODO: generate a list with accounts
+# TODO: print two elements from the list and ask user which one is higher or lower for him
+# TODO: write a functionn
