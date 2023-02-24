e24726b2b4607fb7102a5745287ab005d8384ef6
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-02-24 23:47:36 +0100

    test

--- higher_lower.py
+++ higher_lower.py
@@ -1,4 +1,6 @@
-# TODO: Generate a random number of followers for each element in the list
-# TODO: generate a list with accounts
-# TODO: print two elements from the list and ask user which one is higher or lower for him
-# TODO: write a functionn
+# todo: "Generate a random number of followers for each element in the list"
+# todo: "generate a list with accounts"
+# todo: "print two elements from the list and ask user which one is higher or lower for him"
+# todo: "write a functionn"
+
+#https://www.jetbrains.com/help/pycharm/using-todo.html
