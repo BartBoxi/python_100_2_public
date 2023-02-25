4d7c95ec69e7af59072cdb53d748bd5aa0664386
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-02-25 23:56:30 +0100

    test

--- higher_lower_game/main.py
+++ higher_lower_game/main.py
@@ -6,3 +6,19 @@
 #https://www.jetbrains.com/help/pycharm/using-todo.html
 import random
 
+from art import logo
+from game_data import data
+print(logo)
+
+# lista = random.choice(data['name'])
+# print(lista)
+
+for person in data:
+    nowa_lista = []
+    nowa_lista.append(person['name'])
+    nowa_lista.append(person['follower_count'])
+
+
+
+
+
