825e30b6fdbacdd2259e1b3ed36b134fed2fd1a6
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-02-28 23:40:11 +0100

    Merge remote-tracking branch 'origin/main'

--- higher_lower_game/main.py
+++ higher_lower_game/main.py
@@ -1,68 +1,67 @@
-# todo: "Generate a random number"
-# todo: "generate a list with accounts"
-# todo: "print two elements from the list and ask user which one is higher or lower for him"
-# todo: "write a functionn"
-
-#https://www.jetbrains.com/help/pycharm/using-todo.html
-import random
-
-from art import logo
 from game_data import data
-print(logo)
-
-#Generate a radnom account from the game
-
-account_a = random.choice(data)
-account_b = random.choice(data)
-if account_a == account_b:
-    account_b = random.choice(data)
-print(account_a)
-
-#Format the account data into printable format
-
-account_name = account_a["name"]
-account_description = account_a["description"]
-account_country = account_a["country"]
-
-print(f"Account A is {account_name}, a {account_description} from {account_country}")
-
-account_name_b = account_b['name']
-account_description_b = account_b["description"]
-account_country_b = account_b["country"]
-
-print(f"Account B is {account_name_b}, a {account_description_b} from {account_country_b}")
+import random
+from art import logo, vs
+import os
 
 
-#Ask user for a guess
-
-answer = input("Guess which account got more subscribers. Account A or B? Enter 'A' or 'B' ")
-
-#Check if user is correct
-
-if answer == "A" and (account_a["follower_count"] > account_b["follower_count"]):
-    print("You are right")
-else:
-    print("You are wrong")
-
-#Get follower count of each account
+def get_random_account():
+    """Get data from random account"""
+    return random.choice(data)
 
 
-
-# Use if statement to check if user is correct
-
-#Score keeping
-
-#Make the game repeatable
-
-#Making account at position B become the next account at position A
-
-# clear the screen between rounds
+def format_data(account):
+    """Format account into printable format: name, description and country"""
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    # print(f'{name}: {account["follower_count"]}')
+    return f"{name}, a {description}, from {country}"
 
 
+def check_answer(guess, a_followers, b_followers):
+    """Checks followers against user's guess
+    and returns True if they got it right.
+    Or False if they got it wrong."""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 
+def game():
+    print(logo)
+    score = 0
+    game_should_continue = True
+    account_a = get_random_account()
+    account_b = get_random_account()
+
+    while game_should_continue:
+        account_a = account_b
+        account_b = get_random_account()
+
+        while account_a == account_b:
+            account_b = get_random_account()
+
+        print(f"Compare A: {format_data(account_a)}.")
+        print(vs)
+        print(f"Against B: {format_data(account_b)}.")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+        #cls()
+        print(logo)
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+        else:
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Final score: {score}")
 
 
-
-
-
+game()
