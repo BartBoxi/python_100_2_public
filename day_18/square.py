36199f1bd131e54190ba26bf2aedf493e47ce821
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-04-10 00:43:49 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	OOP/main.py

---
+++ day_18/square.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("green")
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
