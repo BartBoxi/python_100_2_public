62777bb16aa1277fbd2e0c3e692918eabc82781b
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-04-09 23:05:00 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	OOP/main.py

--- OOP/day17/main.py
+++ OOP/day17/main.py
@@ -2,10 +2,25 @@ class User:
     def __init__(self, user_id, username):
         self.user_id = user_id
         self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 user_1 = User("001", "Barto")
+user_2 = User("002", "Jack")
 
-print(user_1.id)
+print(user_1.user_id)
 print(user_1.username)
+print(user_1.followers)
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
 
 
---
+++ day_18/main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("green")
+timmy_the_turtle.forward(25)
+timmy_the_turtle.right(90)
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
