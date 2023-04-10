e2566da524493a49ea86b3a2acd6923a3600f563
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-04-10 23:01:05 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	OOP/main.py

--- day_18/main.py
+++ day_18/main.py
@@ -1,18 +1,25 @@
-import random
-from turtle import Turtle as t
+
+import turtle as t
 from turtle import *
 from turtle import Screen
 from random import randint
 import random
-tim = t()
+
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
 
 
 for _ in range(100):
-    colors = ["red", "purple", "brown", "pink",
-              "green"]
-    speed(randint(0, 50))
-    pensize(25)
-    pencolor(random.choice(colors))
+    tim.color(random_color())
+    tim.speed(randint(0, 50))
+    tim.pensize(10)
     tim.goto(randint(-360, 360), randint(-360, 360))
 
 
