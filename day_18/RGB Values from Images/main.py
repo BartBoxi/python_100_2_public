1295fff26d45f655c1c511343578c747fe1f6957
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-04-15 00:06:33 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	OOP/main.py

--- day_18/RGB Values from Images/main.py
+++ day_18/RGB Values from Images/main.py
@@ -26,10 +26,24 @@ color_list = [(236, 235, 240), (236, 231, 234), (222, 163, 66), (19, 45, 87), (1
  (231, 172, 190), (57, 71, 39), (197, 102, 134), (197, 125, 150), (156, 191, 185), (30, 67, 58), (236, 245, 241),
  (166, 204, 202), (62, 26, 45), (145, 165, 181), (6, 79, 111), (35, 44, 99), (71, 153, 84), (120, 41, 33),
  (170, 203, 205), (223, 178, 169)]
+x = 0
+y = 0
 
-
-
+# t.setheading(225)
+# t.forward(300)
+# t.setheading(0)
 #10x10 size of the cirlce is 20 and separated by 50 from each other
+for _ in range(10):
+    t.up()
+    t.setheading(225)
+    t.forward(500)
+    t.setheading(0)
+    for _ in range(10):
+        t.dot(20, random.choice(color_list))
+        t.up()
+        t.fd(50)
+    y += 70
+    t.goto(x, y)
 
 
 
