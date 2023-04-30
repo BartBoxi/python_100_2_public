2f350ae42e9e4c65675494364adc5060c64deb62
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-04-30 23:39:42 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	OOP/main.py

--- day_20/main.py
+++ day_20/main.py
@@ -23,7 +23,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 14:
+        food.refresh()
 
 screen.exitonclick()
