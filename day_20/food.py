b8e57da6dacd13b5a095e07e423e5d28370fa2f7
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-04-30 23:24:37 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	OOP/main.py

--- day_20/food.py
+++ day_20/food.py
@@ -6,4 +6,4 @@ class Food(Turtle):
         super().__init__()
         self.shape("circular")
         self.penup()
-        self.shapesize()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
