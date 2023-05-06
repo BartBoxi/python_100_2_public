0338a3fa7e463ce4cb016f8e7f9b41d4298130c2
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-05-07 00:55:59 +0200

    Merge remote-tracking branch 'origin/main'

--- day_20/scoreboard.py
+++ day_20/scoreboard.py
@@ -1,12 +1,28 @@
 from turtle import Turtle
-import random
 
 
 class Scoreboard(Turtle):
 
-    def __int__(self):
+    def __init__(self):
         super().__init__()
         self.score = 0
-        self.write(f"Score = {self.score}", align= "center")
+        self.up()
+        self.color("white")
+        self.goto(0, 270)
+        self.write(f"Score = {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.hideturtle()
+        self.update_scoreboard()
 
+
+
+    def update_scoreboard(self):
+        self.write(f"Score = {self.score}", align="center", font=("Arial", 24, "normal"))
+
+
+
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
 
