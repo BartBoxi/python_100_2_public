4117eb242cb1eb7aa01a0ca92313204baa690c45
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-05-14 23:53:32 +0200

    Merge remote-tracking branch 'origin/main'

--- day_22/main.py
+++ day_22/main.py
@@ -1,11 +1,16 @@
 from turtle import Screen
-
+from turtle import Turtle
 
 screen = Screen()
 screen.setup(width=800, height= 600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+
 
 
 
---
+++ day_22/padle.py
@@ -0,0 +1,25 @@
+# from turtle import Turtle
+# STARTING_POSITION = (350, 0)
+# MOVE_DISTANCE = 20
+#
+#
+#
+# class Padle:
+#
+#     def __init__(self):
+#         self.padle = []
+#         self.create_padle()
+#
+#
+#     def create_padle(self):
+#         for position in STARTING_POSITION:
+#             self.add_padle(position)
+#
+#     def add_padle(self, position):
+#         new_padle = Turtle(shape="square")
+#         new_padle = Turtle.turtlesize(stretch_wid=20,stretch_len =100)
+#         new_padle.color("white")
+#         new_padle.up()
+#         new_padle.goto(position)
+#         self.padle.append(new_padle)
+#
