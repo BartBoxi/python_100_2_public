9e2de3bf0135123e927c7c1fed196e510f9ad991
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-05-16 23:58:56 +0200

    Merge remote-tracking branch 'origin/main'

--- day_22/main.py
+++ day_22/main.py
@@ -1,34 +1,18 @@
 from turtle import Screen, Turtle
-
+from padle import Paddle
 screen = Screen()
 screen.setup(width=800, height= 600)
 screen.bgcolor("black")
 screen.title("Pong")
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-screen.tracer(0)
-paddle.goto(350, 0)
+right_position = (350,0)
+left_position = (-350,0)
 
+r_paddle = Paddle(right_position)
+l_paddle = Paddle(left_position)
 
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
-
+screen.tracer()
 game_is_on = True
-
 while game_is_on:
     screen.update()
 
