8763b18cea63ac7e749b40764a7186f1d77f701d
Bart Box <bartoszpudlo07@gmail.com>
2023-07-03 23:41:40 +0200

    Merge remote-tracking branch 'origin/main'

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -25,3 +25,10 @@ while game_is_on:
     screen.update()
     car_manager.create_car()
     car_manager.move_cars()
+
+    # Detect colision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+
