bf1e3a2e3e5f04bd265040d7e1fddcd8fd08302b
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-07-05 22:50:19 +0200

    Merge remote-tracking branch 'origin/main'

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -8,32 +8,32 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-STARTING_POSITION = (0, -280)
-
-player = Player((STARTING_POSITION))
+player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(player.up, "Up")
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
     car_manager.create_car()
     car_manager.move_cars()
 
-    # Detect collision with car
+    #Detect collision with car
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
-    #Detect crosing the whole game
+    #Detect successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
-    else:
-        game_is_on = False
+        car_manager.level_up()
+        scoreboard.increase_level()
+
 
 screen.exitonclick()
-
-
