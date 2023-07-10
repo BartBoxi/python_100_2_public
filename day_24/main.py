de6317b5531b2a17eb9ca7b6d5ecdfb41d17d5ee
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-07-10 23:54:40 +0200

    Merge remote-tracking branch 'origin/main'

--- day_24/main.py
+++ day_24/main.py
@@ -1,4 +1,18 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
-file.close()
+#this is a first method to read the file
+
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+#this is a second method to read file
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# with open("my_file.txt", mode="a") as file: #mode allows the change what you can do with file w =write a=append
+#     file.write("\nNew text.")
+#     file.write("\nHello worlds")
+
+with open("new_file.txt", mode="w") as file:
+    file.write("\nHello worlds")
