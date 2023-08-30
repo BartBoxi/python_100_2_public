9ab2c991d5f6a3d319c82dbd6177f6a5fd86b71d
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-08-30 22:14:12 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ day_27/adding.py
@@ -0,0 +1,7 @@
+def add(*args):
+    suma = 0
+    for n in args:
+        suma += int(n)
+
+    print(suma)
+add(2,1,1)
--- day_27/main.py
+++ day_27/main.py
@@ -4,8 +4,3 @@
 # print(foo(4,8,9))
 
 
-def add(*args):
-    for n in args:
-        print(n)
-
-add(2,1,1)
