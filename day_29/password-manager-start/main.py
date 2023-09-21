5d77a09e4a7c2a583505c5541746aa7881f1742d
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-09-21 10:01:01 +0200

    Merge remote-tracking branch 'origin/main'

--- day_29/password-manager-start/main.py
+++ day_29/password-manager-start/main.py
@@ -1,6 +1,19 @@
+from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+window = Tk()
+window.title("Password Manager")
+window.minsize(width=300, height=300)
+
+canvas = Canvas(width=200, height=200, bg="#808000", highlightbackground="#808000")
+img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=img)
+canvas.pack()
+
+window.mainloop()
