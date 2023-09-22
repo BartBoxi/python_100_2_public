b69ff16abda2350740bc582fbbc581aceef36c28
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-09-22 10:01:09 +0200

    Merge remote-tracking branch 'origin/main'

--- day_29/password-manager-start/main.py
+++ day_29/password-manager-start/main.py
@@ -11,9 +11,10 @@ window = Tk()
 window.title("Password Manager")
 window.minsize(width=300, height=300)
 
-canvas = Canvas(width=200, height=200, bg="#808000", highlightbackground="#808000")
+canvas = Canvas(width=200, height=200, bg="#FFFFFF", highlightbackground="#FFFFFF")
 img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=img)
 canvas.pack()
+window.config(padx=20, pady=20)
 
 window.mainloop()
