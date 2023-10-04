515956031bf2a146c41549a1093c089aa831d348
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-10-04 23:04:50 +0200

    Merge remote-tracking branch 'origin/main'

--- day_29/password-manager-start/main.py
+++ day_29/password-manager-start/main.py
@@ -6,7 +6,18 @@ from tkinter import *
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def add():
-    print()
+    website = website_entry.get()
+    email = email_entry.get()
+    password = pass_entry.get()
+
+    with open("saved_password.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        pass_entry.delete(0, END)
+
+
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -27,8 +38,9 @@ website_label.grid(column = 0, row=1)
 
 #Website entry
 
-webstite_entry = Entry(width = 35 )
-webstite_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width = 35)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 
 email_label = Label(text="Email/Username",font=("Courier", 25))
@@ -38,6 +50,7 @@ email_label.grid(column=0, row=3)
 
 email_entry = Entry(width = 35 )
 email_entry.grid(column=1, row=3, columnspan=2)
+email_entry.insert(0, "bartoszpudlo07@gmail.com")
 
 password_label = Label(text="Password",font=("Courier", 25))
 password_label.grid(column=0, row=4)
@@ -46,6 +59,7 @@ password_label.grid(column=0, row=4)
 
 pass_entry = Entry(width = 21)
 pass_entry.grid(column=1, row=4)
+
 
 def pass_generation():
     return 0
