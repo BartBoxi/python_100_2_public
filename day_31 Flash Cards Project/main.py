4df1b057cdc9bd2818a946fb5628c7ff1887b55e
bartoszpudlo <bartoszpudlo07@gmail.com>
2024-01-07 23:44:21 +0100

    Merge remote-tracking branch 'origin/main'

--- day_31 Flash Cards Project/main.py
+++ day_31 Flash Cards Project/main.py
@@ -2,20 +2,29 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 from tkinter import *
 
-card_back = PhotoImage(file="images/card_back.png")
-card_front = PhotoImage(file="images/card_front.png")
-right = PhotoImage(file="/images/right.png")
-wrong = PhotoImage(file="/images/wrong.png")
+
 
 window = Tk()
 window.title("Flashy")
-window.config(width=1000, height=700, bg="#B1DDC6")
-canvas = Canvas(width=800, height=526, bg="#B1DDC6")
-canvas.create_image(800, 526, image=card_front)
-canvas.grid(row=0, column=0)
+window.config(padx=50, pady=50, bg="#B1DDC6")
+canvas = Canvas(width=800, height=526)
+card_back = PhotoImage(file="images/card_back.png")
+card_front = PhotoImage(file="images/card_front.png")
+canvas.create_image(400,263, image=card_front)
+canvas.create_text(400, 150, text="title",fill="black", font=("Ariel",40, "italic"))
+canvas.create_text(400, 223, text="Word",fill="black", font=("Ariel",60, "bold"))
+canvas.config(bg="#B1DDC6", highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
 
+right = PhotoImage(file="/Users/bartoszpudlo/PycharmProjects/python_100_2/day_31 Flash Cards Project/images/right.png")
+wrong = PhotoImage(file="/Users/bartoszpudlo/PycharmProjects/python_100_2/day_31 Flash Cards Project/images/wrong.png")
 
+unknown_button = Button(image=wrong, highlightthickness=0)
+unknown_button.grid(row=1, column=0)
+
+good_button = Button(image=right, highlightthickness=0)
+good_button.grid(row=1, column=1)
 
 
 window.mainloop()
