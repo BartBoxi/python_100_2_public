4bad7628e4d5cf6f83d3df8604c0ac244be19baf
bartoszpudlo <bartoszpudlo07@gmail.com>
2024-01-11 23:37:13 +0100

    Merge remote-tracking branch 'origin/main'

--- day_31 Flash Cards Project/main.py
+++ day_31 Flash Cards Project/main.py
@@ -1,14 +1,24 @@
 BACKGROUND_COLOR = "#B1DDC6"
 
 from tkinter import *
-import pandas
+import pandas as pd
+import random
 
+data = pd.read_csv('data/french_words.csv')
+to_learn = data.to_dict(orient='records')
 
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text = current_card["French"])
+    canvas.itemconfig(card_word, text = current_card["English"])
 
+def next_french_word():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text=current_card["French"])
+    #if next_card()
 
-
+# TODO: add functionaly when from next french card im going to next card to show first the translation of uknown word
 #---------------------------------DATA---------------------------------#
-
 
 
 
@@ -20,8 +30,8 @@ canvas = Canvas(width=800, height=526)
 card_back = PhotoImage(file="images/card_back.png")
 card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400,263, image=card_front)
-canvas.create_text(400, 150, text="title",fill="black", font=("Ariel",40, "italic"))
-canvas.create_text(400, 223, text="Word",fill="black", font=("Ariel",60, "bold"))
+card_title = canvas.create_text(400, 150, text="title", fill="black", font=("Ariel",40, "italic"))
+card_word = canvas.create_text(400, 223, text="word",fill="black", font=("Ariel",60, "bold"))
 canvas.config(bg="#B1DDC6", highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -29,10 +39,10 @@ canvas.grid(row=0, column=0, columnspan=2)
 right = PhotoImage(file="/Users/bartoszpudlo/PycharmProjects/python_100_2/day_31 Flash Cards Project/images/right.png")
 wrong = PhotoImage(file="/Users/bartoszpudlo/PycharmProjects/python_100_2/day_31 Flash Cards Project/images/wrong.png")
 
-unknown_button = Button(image=wrong, highlightthickness=0)
+unknown_button = Button(image=wrong, highlightthickness=0, command = next_card)
 unknown_button.grid(row=1, column=0)
 
-good_button = Button(image=right, highlightthickness=0)
+good_button = Button(image=right, highlightthickness=0, command= next_french_word)
 good_button.grid(row=1, column=1)
 
 
