62e78254fde37668a53644dfd16fa7f7c9ebbd69
bartoszpudlo <bartoszpudlo07@gmail.com>
2024-01-12 22:24:03 +0100

    Merge remote-tracking branch 'origin/main'

--- day_31 Flash Cards Project/main.py
+++ day_31 Flash Cards Project/main.py
@@ -7,15 +7,32 @@ import random
 data = pd.read_csv('data/french_words.csv')
 to_learn = data.to_dict(orient='records')
 
-def next_card():
-    current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text ="French")
-    canvas.itemconfig(card_word, text = current_card["French"])
+current_card = {}
 
-def next_french_word():
+
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_word, text=current_card["French"])
-    #if next_card()
+    canvas.itemconfig(card_title, text ="French", fill="black")
+    canvas.itemconfig(card_word, text = current_card["French"], fill="black")
+    canvas.itemconfig(canvas_image, image=card_front)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+
+
+def flip_card():
+    canvas.itemconfig(canvas_image, image=card_back)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+
+# def next_french_word():
+#     current_card = random.choice(to_learn)
+#     canvas.itemconfig(card_word, text=current_card["French"])
+#     #if next_card()
 
 # TODO: add functionaly when from next french card im going to next card to show first the translation of uknown word
 #---------------------------------DATA---------------------------------#
@@ -26,10 +43,14 @@ def next_french_word():
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg="#B1DDC6")
+
+
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
 card_back = PhotoImage(file="images/card_back.png")
 card_front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400,263, image=card_front)
+canvas_image = canvas.create_image(400,263, image=card_front)
 card_title = canvas.create_text(400, 150, text="title", fill="black", font=("Ariel",40, "italic"))
 card_word = canvas.create_text(400, 223, text="word",fill="black", font=("Ariel",60, "bold"))
 canvas.config(bg="#B1DDC6", highlightthickness=0)
@@ -42,7 +63,7 @@ wrong = PhotoImage(file="/Users/bartoszpudlo/PycharmProjects/python_100_2/day_31
 unknown_button = Button(image=wrong, highlightthickness=0, command = next_card)
 unknown_button.grid(row=1, column=0)
 
-good_button = Button(image=right, highlightthickness=0, command= next_french_word)
+good_button = Button(image=right, highlightthickness=0, command= next_card)
 good_button.grid(row=1, column=1)
 
 next_card()
