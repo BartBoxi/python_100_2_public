a736e9494205cc97384e4ac61defe7ec25ebf9fb
Bartosz-MacMini <bartoszpudlo07@gmail.com>
2024-01-15 23:46:42 +0100

    Create README.txt

--- day_32/birthday-wisher-normal-start/main.py
+++ day_32/birthday-wisher-normal-start/main.py
@@ -1,8 +1,9 @@
 ##################### Normal Starting Project ######################
-
+import random
 import smtplib
 import datetime as dt
 import pandas as pd
+import random as rd
 
 now = dt.datetime.now()
 today = now.date()
@@ -15,23 +16,51 @@ data = pd.read_csv("birthdays.csv")
 birthday_dict = {(data_row["month"], data_row["day"]): (f"{data_row["name"]},{data_row["email"]},{data_row["year"]},"
                                                         f"{data_row["month"]}, {data_row["day"]}")
                                                         for (index, data_row) in data.iterrows()}
-print(birthday_dict)
+#print(birthday_dict)
+
+if (today_month, today_day) in birthday_dict:
+    today_birthday_info = birthday_dict[(today_month, today_day)]
+    name = today_birthday_info.split(',')[0]
+    email = today_birthday_info.split(',')[1]
+    print(today_birthday_info)
+    print(name)
+    letter_number = random.randint(1,3)
+    if letter_number == 1:
+        with open('letter_templates/letter_1.txt', 'r') as file:
+            content = file.read()
+            content = content.replace("[Name]", name)
+        with open('letter_templates/letter_1.txt', 'a+') as file:
+            file.write(content)
+
+
+    elif letter_number ==2:
+        with open('letter_templates/letter_2.txt', 'r') as file:
+            content = file.read()
+            content = content.replace("[NAME]", name)
+        with open('letter_templates/letter_2.txt', 'a+') as file:
+            file.write(content)
+
+
+    elif letter_number ==3:
+        with open('letter_templates/letter_3.txt', 'r') as file:
+            content = file.read()
+            content = content.replace("[Name]", name)
+        with open('letter_templates/letter_3.txt', 'a+') as file:
+            file.write(content)
 
 
 
-#HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
+def send_email():
+    my_email = "pythonbarttest@gmail.com"
+    password = "udyd dkwa cost ydqy"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs={email},
+                            msg=f'Subject:Happy Birthday\n\n{content}.')
 
-# 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT 1: Think about the relative file path to open each letter. 
-# HINT 2: Use the random module to get a number between 1-3 to pick a randome letter.
-# HINT 3: Use the replace() method to replace [NAME] with the actual name. https://www.w3schools.com/python/ref_string_replace.asp
+send_email()
 
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
-# HINT 2: Remember to call .starttls()
-# HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
-# HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
 
 
 
