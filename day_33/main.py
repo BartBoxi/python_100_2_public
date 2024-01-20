06d179745886ecb72d259293b8e9c34623522d03
Bartosz-MacMini <bartoszpudlo07@gmail.com>
2024-01-20 23:12:14 +0100

    Create README.txt

--- day_33/main.py
+++ day_33/main.py
@@ -1,11 +1,31 @@
 import requests
+from datetime import datetime
+MY_LAT = 52.242130
+MY_LONG = 20.912830
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_positon = (longitude, latitude)
+# print(iss_positon)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
 
-iss_positon = (longitude, latitude)
-print(iss_positon)
+time_now = datetime.now()
+print(time_now)
+
+print(sunset, sunrise)
