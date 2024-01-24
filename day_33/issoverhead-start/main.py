3b2e3b58995c91f591307dec755dcc03a33e2ce6
Bartosz-MacMini <bartoszpudlo07@gmail.com>
2024-01-24 22:08:45 +0100

    Create README.txt

--- day_33/issoverhead-start/main.py
+++ day_33/issoverhead-start/main.py
@@ -1,8 +1,14 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
 MY_LAT = 51.507351 # Your latitude
 MY_LONG = -0.127758 # Your longitude
+
+
+# MY_LAT = 28.507351 # Your latitude
+# MY_LONG = -99.127758 # Your longitude
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -27,10 +33,33 @@ sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
+time_now = time_now.strftime('%H')
 
+# print(sunrise)
+# print(sunset)
+# print(time_now)
 #If the ISS is close to my current position
 
-if iss_latitude
+message = f"Look up! ISS is above you with latitude {iss_latitude} and longitude {iss_longitude}"
+def send_email():
+    my_email = "pythonbarttest@gmail.com"
+    password = "udyd dkwa cost ydqy"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs="bartoszpudlo07@gmail.com",
+                            msg=f'Subject:ISS is above\n\n{message}.')
+
+while True:
+#The ISS position is within +5 or -5 degrees of the iss position
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG-5:
+        if sunset <= time_now <= sunrise:
+            send_email()
+    else:
+        print("nah")
+    time.sleep(60)
+
+
 
 # and it is currently dark
 # Then send me an email to tell me to look up.
