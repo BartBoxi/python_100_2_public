06a146f823926a9be92798d854b3b9956eb11321
Bartosz-MacMini <bartoszpudlo07@gmail.com>
2024-02-04 22:50:08 +0100

    Create README.txt

--- day_35/main.py
+++ day_35/main.py
@@ -1,18 +1,17 @@
-<<<<<<< HEAD
+
 import requests
 
 endpoint = "http://api.openweathermap.org/data/2.5/forecast"
 weather_param = {
     "appid": "fe4710a7303b544c44b9c3041b99d647",
     "q": "Warsaw",
+    "cnt": 4
 }
 response = requests.get(endpoint, params=weather_param)
 
 print(response.status_code)
-
+print(response.raise_for_status())
 data = response.json()
 print(data)
-=======
-api_keu = "8f1ba3f2663b7bf0011d4ab86989fa29"
 
->>>>>>> origin/main
+
