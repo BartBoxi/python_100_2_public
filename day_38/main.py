9d7b085c65d45d4dd0f45afc379660f5926b98db
Bartosz-MacMini <bartoszpudlo07@gmail.com>
2024-02-26 23:33:10 +0100

    Create README.txt

---
+++ day_38/main.py
@@ -0,0 +1,19 @@
+import requests
+
+
+
+APP_ID = "024a7bf0"
+API_KEY = "cd9678b13f4b908b97e0c13f75672ccc"
+
+headers = {
+    "Content-Type": "application/json",
+    "x-app-id" : APP_ID,
+    "x-app-key" : API_KEY,
+}
+excercise = input("Enter the excercise you did ")
+url = "https://trackapi.nutritionix.com/v2/natural/excercise"
+endpoint = f"{url}/{excercise}"
+
+excercise_put = requests.put(url=endpoint, headers=headers)
+print(excercise_put.text)
+print(excercise_put.json)
