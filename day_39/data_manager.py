eee6ff99edc3fabbcb1b3b068f92c4ec96845ec6
Bartosz-MacMini <bartoszpudlo07@gmail.com>
2024-03-20 23:35:06 +0100

    Create README.txt created a authorization method

---
+++ day_39/data_manager.py
@@ -0,0 +1,36 @@
+import requests
+
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+    sheety_url = "https://api.sheety.co/c5294e1cd882deb45fb05bdb1c1640b8/flightDeals/prices/2"
+    data = {
+        "price": {
+            "iataCode": "WAW"
+        }
+    }
+    headers_sheety = {"Authorization": "Basic YmFydDpNZW5hY29yKiExOTk2"}
+    endpoint = f"{sheety_url}"
+    response = requests.put(url=endpoint, json = data, headers=headers_sheety)
+    print("response.status_code =", response.status_code)
+    print("response.text= ", response.text)
+
+DataManager()
+
+
+
+#
+# import requests
+# SHEETY_ENDPOINT = f'https://api.sheety.co/xxxxxxxxxxxxxxxx/sheetyTest/sheet1/'
+#
+# id = 2
+# data = {
+#     "sheet1": {
+#         "a": "C",
+#         "b": "D",
+#     }
+# }
+# endpoint = f"{SHEETY_ENDPOINT}/{id}"
+#
+# response = requests.put(url=endpoint, json=data)
+# print("response.status_code =", response.status_code)
+# print("response.text= ", response.text)
--- day_39/main.py
+++ day_39/main.py
@@ -1,4 +1,7 @@
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
 import requests
+from pprint import pprint
 import os
 import json
 
@@ -12,26 +15,33 @@ sheety_url = "https://api.sheety.co/c5294e1cd882deb45fb05bdb1c1640b8/flightDeals
 # PASS = os.environ["PASS"]
 headers_sheety = {"Authorization":"Basic YmFydDpNZW5hY29yKiExOTk2"}
 response = requests.get(url=sheety_url)
-data = response.json()
+sheet_data = response.json()
+sheet_data = sheet_data["prices"]
+#print("response.status_code = ", response.status_code)
+#print("response.text= ", response.text)
+pprint(sheet_data)
 
-print("response.status_code = ", response.status_code)
-print("response.text= ", response.text)
-print(data)
 
 
-##kiwi_search_api
-kiwi_search_url = "https://api.tequila.kiwi.com/v2"
 
-parameters = {
-    "apikey" : "QLYELueajtR5B7uG9le9s4MZ93Gj1w54",
-    "fly_from" : "WAW",
-    "fly_to" : "SFO",
-    "date_from" : "20/03/2024",
-    "date_to" : "01/05/2024"
-}
 
-flights = requests.get(url=kiwi_search_url, params=parameters)
-flights_data = flights.json()
-print("response.status_code = ", flights.status_code)
-print("response.text= ", flights.text)
-print(flights_data)
+
+
+
+
+# ##kiwi_search_api
+# kiwi_search_url = "https://api.tequila.kiwi.com/v2"
+#
+# parameters = {
+#     "apikey" : "QLYELueajtR5B7uG9le9s4MZ93Gj1w54",
+#     "fly_from" : "WAW",
+#     "fly_to" : "SFO",
+#     "date_from" : "20/03/2024",
+#     "date_to" : "01/05/2024"
+# }
+#
+# flights = requests.get(url=kiwi_search_url, params=parameters)
+# flights_data = flights.json()
+# print("response.status_code = ", flights.status_code)
+# print("response.text= ", flights.text)
+# print(flights_data)
