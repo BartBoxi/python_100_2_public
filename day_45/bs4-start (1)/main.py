983f9a79a8d05efd5f08d1cb0cce6dc7b847c682
bartoszpudlo <bartoszpudlo07@gmail.com>
2024-04-07 23:48:38 +0200

    scraping hacker news page to get title of article and link

--- day_45/bs4-start (1)/main.py
+++ day_45/bs4-start (1)/main.py
@@ -1,8 +1,28 @@
 from bs4 import BeautifulSoup
-import lxml
+# import lxml
+#
+# with open("website2.html") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
+# print(soup.title.string)
+# print(soup.title)
+# print(soup.prettify())
 
-with open("website2.html") as file:
-    contents = file.read()
+import requests
 
-soup = BeautifulSoup(contents, "html.parser")
-print(soup.title.string)
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+print(soup.title)
+#article_tag = soup.find(name="span", class_="storylink")
+
+article_tag = soup.find("span", class_="titleline")
+article_text = article_tag.getText()
+article_link = article_tag.find('a')
+article_link = article_link.get("href")
+
+print(article_tag)
+print(article_text)
+print(article_link)
