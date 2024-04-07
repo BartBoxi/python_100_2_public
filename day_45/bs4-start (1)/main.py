0ef93f7ba5bae1033ddbbb75126c6d181aca5c5c
bartoszpudlo <bartoszpudlo07@gmail.com>
2024-04-07 23:48:38 +0200

    adding css

--- day_45/bs4-start (1)/main.py
+++ day_45/bs4-start (1)/main.py
@@ -1,8 +1,21 @@
 from bs4 import BeautifulSoup
-import lxml
+# import lxml
+#
+# with open("website2.html") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
+# print(soup.title.string)
+# print(soup.title)
+# print(soup.prettify())
 
-with open("website2.html") as file:
-    contents = file.read()
+import requests
 
-soup = BeautifulSoup(contents, "html.parser")
-print(soup.title.string)
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+print(soup.title)
+#article_tag = soup.find(name="span", class_="titleline")
+article_tag = soup.find("span", class_="titleline")
+print(article_tag)
