f86a1b4c814d5ebf5e8b94a7c4d233a111242bbd
Bartosz-MacMini <bartoszpudlo07@gmail.com>
2024-04-21 22:09:38 +0200

    Create README.txt created a authorization method

--- day_45/bs4-start (1)/100_movies/Starting Code - 100 movies to watch start/main.py
+++ day_45/bs4-start (1)/100_movies/Starting Code - 100 movies to watch start/main.py
@@ -4,5 +4,5 @@ from bs4 import BeautifulSoup
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
-
+print("hello")
 
