d277dd986aeb2d49aa2e3817bdbbfc1dfe0906b7
Bartosz-MacMini <bartoszpudlo07@gmail.com>
2024-06-10 23:44:43 +0200

    selenium day 1

--- day_48/main.py
+++ day_48/main.py
@@ -1,3 +1,11 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+# keep chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver=webdriver.Chrome(options=chrome_options)
+
+driver.get("https://amazon.com")
+
+driver.close() #closing single tab
+driver.quit() #quit the entier browser not just a tab
