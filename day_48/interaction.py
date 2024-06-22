c4dbe074de3d76fe3bbd4c335c32273f4b4a47f9
Bart <bartoszpudlo07@gmail.com>
2024-06-22 23:58:58 +0200

    selenium day 4

---
+++ day_48/interaction.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+article_number = driver.find_element(By.XPATH, "//a[@href='/wiki/Special:Statistics']").text
+print(article_number)
