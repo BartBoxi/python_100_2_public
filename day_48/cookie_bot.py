8d25e6c1bc32ba83aaf08015efbfea985affb1f2
Bart <bartoszpudlo07@gmail.com>
2024-07-07 23:56:04 +0200

    cookie project - reading elements from the right panel

--- day_48/cookie_bot.py
+++ day_48/cookie_bot.py
@@ -13,40 +13,38 @@ money = driver.find_element(By.ID, "money")
 
 cursor = driver.find_element(By.ID, "buyCursor")
 cursor_text = cursor.text
-cursor_split = cursor_text.split()
-cursor_price = cursor_split[2]
-
 grandma = driver.find_element(By.ID, "buyGrandma")
 grandma_text = grandma.text
-grandma_split = grandma_text.split()
-grandma_price = grandma_split[2]
 
 factory = driver.find_element(By.ID, "buyFactory")
+factory_text = factory.text
 
 mine = driver.find_element(By.ID, "buyMine")
+mine_text = mine.text
 
 shipment = driver.find_element(By.ID, "buyShipment")
+shipment_text = shipment.text
 
 lab = driver.find_element(By.ID, "buyAlchemy lab")
+lab_text = lab.text
+
 portal = driver.find_element(By.ID, "buyPortal")
+portal_text = portal.text
+
 machine = driver.find_element(By.ID, "buyTime machine")
+machine_text = machine.text
+
+upgrade_elements = [cursor, grandma, factory, mine, shipment, lab, portal, machine]
+
+upgrade_prices = {}
+for name, element in upgrade_elements.items():
+    text = element.text
+    price = int(text.split()[-1].replace(",",""))
+    upgrade_prices[name] = price
+
 
 
 last_execution_time = time.time()
-
-while True:
-    cookie.click()
-    current_time = time.time()
-    if current_time - last_execution_time > 5:
-        money = money.text
-
-        if money > cursor_price:
-            cursor.click()
-        else:
-            pass
-
-
-
 
 
 
--- day_48/test_2.py
+++ day_48/test_2.py
@@ -3,19 +3,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-
-
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, "cookie")
-money = driver.find_element(By.ID, "money")
-money = money.text
 
 
 
-print(item_ids)
+
+timeout = time.time() + 60 * 0.1 #5 min from the start
+
+while True:
+    test = 0
+    cookie.click()
+    money = driver.find_element(By.ID, "money")
+    money = money.text
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value="#store div")
+
+    item_ids = [item.get_attribute("id") for item in items]
 
 
+
+
+    if test == 5 or time.time() > timeout:
+        break
+    test = test - 1
