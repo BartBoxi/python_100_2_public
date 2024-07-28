925279b6a39a5bedd571601213d8c3d3b6f05214
Bart <bartoszpudlo07@gmail.com>
2024-07-28 22:25:53 +0200

    Final code done:
    try / exception was needed for handling price reading.

--- day_48/cookie_bot.py
+++ day_48/cookie_bot.py
@@ -1,16 +1,17 @@
 from selenium import webdriver
+from selenium.common import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
-
-
-
 
 #elements of the page
 cookie = driver.find_element(By.ID, "cookie")
@@ -20,24 +21,24 @@ items = driver.find_elements(By.CSS_SELECTOR, "#store div")
 item_ids = [item.get_attribute("id") for item in items]
 
 timeout = time.time() + 5
-five_min = time.time() + 60 * 0.1
+five_min = time.time() + 60 * 5
 
 
 while True:
     cookie.click()
-
     if time.time() > timeout:
 
-        all_prices = driver.find_elements(By.CSS_SELECTOR, "#store b")
+        all_prices = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#store b")))
         item_prices = []
 
-        for price in all_prices:
-            price_text = price.text
-            if price_text:
-                final_price = price_text.split(" - ")
-                final_price = final_price[1].replace(",","")
-                final_price = int(final_price)
-                item_prices.append(final_price)
+        for price in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#store b"))):
+            try:
+                element_text = price.text
+                if element_text != "":
+                    cost = int(element_text.split("-")[1].strip().replace(",", ""))
+                    item_prices.append(cost)
+            except StaleElementReferenceException:
+                print("Stale element encountered. Retrying...")
 
         #print(len(item_prices))
 
@@ -68,21 +69,22 @@ while True:
         def higher_values(dictionary, threshold):
 
             affordable_items = {}
-            for item_id, price in dictionary.items():  # Iterate through key-value pairs
-                if price < threshold:
-                    affordable_items[item_id] = price
-                    highest_value = max(affordable_items.values())
-                    highest_key = max(affordable_items, key=affordable_items.get)
-                    print(highest_key)
+            for item_id, cena in dictionary.items():  # Iterate through key-value pairs
+                if cena < threshold:
+                    affordable_items[item_id] = cena
+            if affordable_items:
+                highest_value = max(affordable_items.values())
+                highest_key = max(affordable_items, key=affordable_items.get)
+                button = driver.find_element(By.ID, f"{highest_key}")
+                button.click()
+            else:
+                print("no affordable items")
 
             #print(affordable_items)
 
 
         higher_values(item_dict, cookie_count)
-
-
-
-    if time.time() > five_min:
+    elif time.time() > five_min:
         break
 
 
