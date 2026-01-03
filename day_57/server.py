1bab9059c417f565a04f8fa17014065591db76d7
bartoszpudlo <bartoszpudlo07@gmail.com>
2026-01-04 00:59:50 +0100

    Added api for genderize. Still got some stuff to finish with this code.

--- day_57/server.py
+++ day_57/server.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
+import random
+import datetime 
+from faker import Faker
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+
+def home():
+    random_number = random.randint(1, 10)
+    current_year = datetime.datetime.now().year
+    return render_template('index.html', num = random_number, year = current_year)
+
+def guess():
+    api_key = '456e0cea0b6797ddc24720c1847395ea'
+    name = Faker().name()
+    url = 'https://api.genderize.io?name=peter&apikey={api_key}'
+    response = requests.get(url)
+    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
