0e8c8eb50dc8c300157032672f17d6da470d716e
bartoszpudlo <bartoszpudlo07@gmail.com>
2026-01-04 22:35:47 +0100

    Finalized code with window to provide name.

--- day_57/server.py
+++ day_57/server.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import random
 import datetime 
 from faker import Faker
 import requests
 
 app = Flask(__name__)
+fake = Faker()
 
-@app.route('/')
-
+@app.route('/', methods = ['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        user_name = request.form.get('user_name')
+        return redirect(url_for('guess', name = user_name))
     random_number = random.randint(1, 10)
     current_year = datetime.datetime.now().year
     return render_template('index.html', num = random_number, year = current_year)
@@ -16,29 +19,34 @@ def home():
 @app.route('/guess/<name>')
 def guess(name):
 
-    # api key first 
-    api_key = '456e0cea0b6797ddc24720c1847395ea'
-
-    # name 
-    random_name = fake.first_name()
-    print(random_name)
-
     # url 
-    url = f'https://api.genderize.io?name={random_name}&apikey={api_key}'
+    #url = f'https://api.genderize.io?name={random_name}&apikey={api_key}'
+    url = f'https://api.genderize.io?name={name}'
+    age_url = f'https://api.agify.io?name={name}'
+    nationality_url = f'https://api.nationalize.io/?name={name}'
 
     # response 
     response = requests.get(url)
-    print(response)
     data = response.json()
-    print(data)
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    nationality_response = requests.get(nationality_url)
+    nationality_data = nationality_response.json()
 
     # extracting rest of data
     gender = data.get('gender')
+    age = age_data.get('age')
     prob = data.get('probability', 0)
+
+    countries = nationality_data.get('country', [])
+    if countries:
+        nation = countries[0].get('country_id')
+    else:
+        nation = 'Unknown'
     
     # returing the values to the template
     current_year = datetime.datetime.now().year
-    return render_template('guess.html', name  = random_name, gender = gender, probability = prob, year = current_year) 
+    return render_template('guess.html', name  = name, gender = gender, age = age, nationality = nation, probability = prob, year = current_year) 
 
 
 
