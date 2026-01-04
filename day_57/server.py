4b7cedf6dbbdf8f89d654a98a66ade9971ce9bd7
bartoszpudlo <bartoszpudlo07@gmail.com>
2026-01-04 21:03:56 +0100

    Adding extra changes to code

--- day_57/server.py
+++ day_57/server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import random
 import datetime 
 from faker import Faker
+import requests
 
 app = Flask(__name__)
 
@@ -12,12 +13,33 @@ def home():
     current_year = datetime.datetime.now().year
     return render_template('index.html', num = random_number, year = current_year)
 
-def guess():
+@app.route('/guess/<name>')
+def guess(name):
+
+    # api key first 
     api_key = '456e0cea0b6797ddc24720c1847395ea'
-    name = Faker().name()
-    url = 'https://api.genderize.io?name=peter&apikey={api_key}'
+
+    # name 
+    random_name = fake.first_name()
+    print(random_name)
+
+    # url 
+    url = f'https://api.genderize.io?name={random_name}&apikey={api_key}'
+
+    # response 
     response = requests.get(url)
+    print(response)
+    data = response.json()
+    print(data)
+
+    # extracting rest of data
+    gender = data.get('gender')
+    prob = data.get('probability', 0)
     
+    # returing the values to the template
+    current_year = datetime.datetime.now().year
+    return render_template('guess.html', name  = random_name, gender = gender, probability = prob, year = current_year) 
+
 
 
 
